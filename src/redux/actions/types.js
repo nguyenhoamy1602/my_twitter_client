b4e7375ea78c6f8ee0564100ea4f25f3a08eebf8
nguyen.hoamy <nguyen.hoamy@hde.co.jp>
2018-07-05 11:21:40 +0900

    Change url

--- src/redux/actions/types.js
+++ src/redux/actions/types.js
@@ -5,7 +5,8 @@ export const DELETE_TWEET = 'delete_tweet_from_tweet_list';
 export const NEW_TOAST = 'new_toast';
 export const CLEAR_TOAST = 'clear_toast';
 export const TOKEN = "eyJhbGciOiJIUzI1NiIsImlhdCI6MTUzMDUxMDIzMywiZXhwIjoxNTMxNzE5ODMzfQ.eyJpZCI6IlVzZXI6MSIsIm5hbWUiOiJOZ3V5ZW4gSG9hIE15In0.Ly_7n37O4F-E-Khtku567Roj8PadP4LwuXCIymiLyds";
-export const URL = "http://localhost:5000/api";
+
+export const URL = "http://0.0.0.0:50022/api";
 export const TWEET_URL = URL + "/tweet/"
 export const AUTH_URL = URL + "/auth/"
 
